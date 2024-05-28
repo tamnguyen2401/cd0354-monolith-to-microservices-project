97b85a92d8f867058606a1ad9cbf564fe0befd18
tamnguyen2401 <tamnguyen2401@gmail.com>
2024-05-28 13:33:18 +0700

    deploy

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=tamnguyen2401
 export POSTGRES_PASSWORD=qwerty123456
-export POSTGRES_HOST=uda3.crsx0sinmjav.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=uda3-database.c2auqbexjoq7.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:uda3-microservice-dev
 export AWS_REGION=us-east-1
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a6e215ed7caee406b8bf831e9c1a9e24-120753507.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
